ce73b75bab75455459c392b71acf0a61ece1a893
Helga Lukaj <h.lukaj98@gmail.com>
2017-11-28 17:50:26 +0300

    I made some changes

--- a2_process_data.py
+++ a2_process_data.py
@@ -27,3 +27,28 @@ print("Cell at index 0,1:")
 print(contents[0][1])
 print("Cell at index 1,0:")
 print(contents[1][0])
+print(contents)
+print(contents[0])
+print(contents[0][0])
+print(type(contents))
+print(type(contents[0]))
+print(type(contents[0][0]))
+print(contents[3][3])
+print(contents[23][2])
+av_seat = 0.0
+for i in range(1, 57):
+	av_seat += float(contents[i][1])
+av_seat = av_seat / 56
+print(av_seat)
+dan_airlines=0
+for i in range(1,57):
+	if int(contents[i][2])>=12:
+		dan_airlines += 1
+print(dan_airlines)
+str1 = contents[0][3]+contents[3][1]
+str2 = 2*contents[0][3] 
+print(str1)
+print(str2)
+print(type(str1),type(str2))
+help(type(str1))
+
